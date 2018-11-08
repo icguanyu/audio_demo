36fd406a1f9aec85058633ebc37247894cf57c97
icguanyu <icguanyu@gmail.com>
2018-11-08 11:50:57 +0800

    api test

--- js/all.js
+++ js/all.js
@@ -1,3 +1,5 @@
+// 資料
+let audio_list = []
 // 播放器元素
 const b_play = document.querySelector(".play");
 const b_pause = document.querySelector(".pause");
@@ -15,44 +17,79 @@ const t_profile_img = document.querySelector(".profile img");
 const t_profile_name = document.querySelector(".porfile_info .name");
 const t_profile_intro = document.querySelector(".porfile_info .intro");
 // 狀態
-let onplay = false 
-let currentTime = ''
+let onplay = false;
+let currentTime = "";
 // Visualizations
-let canvas, ctx, source, context, analyser, fbc_array, bars, bar_x
+let canvas, ctx, source, context, analyser, fbc_array, bars, bar_x;
 let audio = new Audio();
-audio.src = 'sounds/sounds.mp3';
-audio.controls = false ; // 不顯示預設播放器
-audio.autoplay = false ;
-function initMp3Player(){
-	document.querySelector('body').appendChild(audio);
-	context = new AudioContext(); // AudioContext object instance
-	analyser = context.createAnalyser(); // AnalyserNode method
-	canvas = document.getElementById('analyser_render');
-	ctx = canvas.getContext('2d');
-	// Re-route audio playback into the processing graph of the AudioContext
-	source = context.createMediaElementSource(audio); 
-	source.connect(analyser);
-	analyser.connect(context.destination);
-	frameLooper();
+// init
+function getData() {
+  console.log('start');
+  fetch('https://infinite-sands-75179.herokuapp.com/api/voices/6')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    audio_list = myJson
+    initlist()
+  });
 }
-function frameLooper(){
-	window.requestAnimationFrame(frameLooper);
-	fbc_array = new Uint8Array(analyser.frequencyBinCount);
-	analyser.getByteFrequencyData(fbc_array);
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = '#fff'; 
-	bars = 200;
-	for (var i = 0; i < bars; i++) {
-		bar_x = i * 2;
-		bar_width = 0.8;
-		bar_height = -(fbc_array[i]/3);
-		ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
-	}
+function initlist() {
+  console.log(audio_list);
+}
+function initMp3Player() {
+  audio.src = "sounds/sounds.mp3";
+  audio.controls = false;
+  audio.autoplay = false;
+  document.querySelector("body").appendChild(audio);
+  context = new AudioContext(); // AudioContext object instance
+  analyser = context.createAnalyser(); // AnalyserNode method
+  canvas = document.getElementById("analyser_render");
+  ctx = canvas.getContext("2d");
+  // Re-route audio playback into the processing graph of the AudioContext
+  source = context.createMediaElementSource(audio);
+  source.connect(analyser);
+  analyser.connect(context.destination);
+  frameLooper();
+}
+// 音訊視覺化 Canvas
+function frameLooper() {
+  window.requestAnimationFrame(frameLooper);
+  fbc_array = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(fbc_array);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#fff";
+  bars = 200;
+  for (var i = 0; i < bars; i++) {
+    bar_x = i * 2;
+    bar_width = 0.8;
+    bar_height = -(fbc_array[i] / 3);
+    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+  }
+}
+// Seconds to H:M:S
+function formatSecond(secs) {
+  let date = new Date(null);
+  date.setSeconds(secs); // specify value for SECONDS here
+  let result = date.toISOString().substr(11, 8);
+  return result;
+}
+// 播放暫停按鈕切換
+function btn_switch() {
+  onplay = !onplay;
+  if (onplay) {
+    // 沒在播放 顯示播放
+    b_play.style.display = "none";
+    b_pause.style.display = "inline-block";
+  } else {
+    // 正在播放 顯示暫停
+    b_play.style.display = "inline-block";
+    b_pause.style.display = "none";
+  }
 }
 
 audio.addEventListener("timeupdate", e => {
   console.log(e);
-
   let progress =
     ((e.target.currentTime / e.target.duration) * 100).toFixed(2) + "%";
   console.log(progress);
@@ -61,47 +98,26 @@ audio.addEventListener("timeupdate", e => {
   t_audio_time_start.textContent = formatSecond(
     e.target.currentTime.toFixed(0)
   );
-  t_audio_time_end.textContent = formatSecond(
-    e.target.duration.toFixed(0)
-  );
+  t_audio_time_end.textContent = formatSecond(e.target.duration.toFixed(0));
 });
 
-// Seconds to H:M:S
-function formatSecond(secs) {
-  let date = new Date(null);
-  date.setSeconds(secs); // specify value for SECONDS here
-  let result = date.toISOString().substr(11, 8);
-  return result
-}
-
-function btn_switch () {
-  onplay = !onplay
-  if(onplay){ // 沒在播放 顯示播放
-    b_play.style.display = 'none'
-    b_pause.style.display = 'inline-block'
-  }else{ // 正在播放 顯示暫停
-    b_play.style.display = 'inline-block'
-    b_pause.style.display = 'none'
-  }
-}
-
-
+window.addEventListener("load", getData);
 window.addEventListener("load", initMp3Player, false);
 
 b_play.addEventListener("click", () => {
   audio.play();
-  btn_switch()
+  btn_switch();
 });
 b_pause.addEventListener("click", () => {
   audio.pause();
-  btn_switch()
+  btn_switch();
 });
 
 b_fast_forward.addEventListener("click", () => {
-  audio.currentTime += 20
+  audio.currentTime += 20;
   audio.play();
 });
 b_fast_rewind.addEventListener("click", () => {
-  audio.currentTime -= 20
+  audio.currentTime -= 20;
   audio.play();
 });
